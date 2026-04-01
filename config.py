9b973edc11f0a906bb8c55985d4ffb5ec8cf56e4
abdalgaderm26 <abdalgadernet1@gmail.com>
2026-04-01 12:37:12 +0400

    Add files via upload

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Bot Token from @BotFather
-TOKEN = os.getenv("BOT_TOKEN", "8622390945:AAHEmKQlnk0PIzrMl7bdMpKiHPNlF6m6yAI")
+TOKEN = os.getenv("BOT_TOKEN", "8622390945:AAHEmKQlnk0PIzrMl7bdMpKiHPNlF6m6yAI").strip()
 
 # Admin ID (Your Telegram ID) - Used for admin commands
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))  # Replace with your Telegram ID if not in .env
